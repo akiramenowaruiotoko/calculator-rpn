876d4da0b7fe2dfa0fa1c544de331a457f927c4a
akiramenowaruiotoko <sawaking@hotmail.co.jp>
2023-12-26 23:31:09 +0900

    create ButtonEx class

---
+++ ButtonEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator_simple
+{
+    internal class ButtonEx : Button
+    {
+        protected override bool IsInputKey(Keys keyData)
+        {
+            // フォーカスが当たっているボタンのエンターキー動作を無視
+            Keys kcode = keyData & Keys.KeyCode;
+            if(kcode == Keys.Enter)
+            {
+                return true;
+            }
+            return base.IsInputKey(keyData);
+        }
+    }
+}
